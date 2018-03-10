7548b1c51b5f8882ffb4e9b032c65fd9bf8fe272
D0radus <allen.tyler.c@gmail.com>
2018-03-09 18:59:44 -0800

    Button functionality
    Can now go from interactive map -> central map -> plant search

--- src/main/java/com/example/steven/myapplication/CentralMap.java
+++ src/main/java/com/example/steven/myapplication/CentralMap.java
@@ -1,7 +1,9 @@
 package com.example.steven.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by tyler.allen925 on 3/7/18.
@@ -12,5 +14,9 @@ public class CentralMap extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.central_map);
+    }
+
+    public void plantSearchButton(View view) {
+        startActivity(new Intent(this, PlantSearch.class));
     }
 }
