b2cb49a795f8b66807589eda5058c2c9e53e8cec
Ricky <rickymedrano4@gmail.com>
2018-03-12 12:33:32 -0700

    constraint
    added constraints on plant spec page formatting

--- src/main/java/com/example/steven/myapplication/AboutPage.java
+++ src/main/java/com/example/steven/myapplication/AboutPage.java
@@ -1,13 +1,13 @@
 package com.example.steven.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class PlantSpec extends AppCompatActivity {
+public class AboutPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plant_spec);
+        setContentView(R.layout.about_page);
     }
 }
--- src/main/java/com/example/steven/myapplication/MainActivity.java
+++ src/main/java/com/example/steven/myapplication/MainActivity.java
@@ -1,25 +1,29 @@
 package com.example.steven.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelperClass myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        myDB = new DatabaseHelperClass(this);
     }
-
-
+    
     public void interactiveMapButton(View view) {
         startActivity(new Intent(this, InteractiveMap.class));
     }
 
     public void plantSearchButton(View view) {
         startActivity(new Intent(this, PlantSearch.class));
+    }
+
+    public void aboutButton(View view){
+        startActivity(new Intent(this, AboutPage.class));
     }
 }
