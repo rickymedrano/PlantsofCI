1fc684145d6529a8a39f997c7061261cdf39d914
Allen <allen.tyler.c@gmail.com>
2018-03-26 13:10:35 -0700

    Google Maps
    Starting implementation of google maps

--- src/main/java/com/example/steven/myapplication/InteractiveMap.java
+++ src/main/java/com/example/steven/myapplication/InteractiveMap.java
@@ -4,19 +4,50 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import java.io.InputStream;
+import
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 /**
  * Created by Tyler on 3/5/2018.
  */
 
 public class InteractiveMap extends Activity {
+
+    protected static final String STATIC_MAP_API_ENDPOINT = "http://maps.google.com/maps/api/staticmap?center=37.4223662,-122.0839445&zoom=15&size=200x200&sensor=false";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.interactive_map);
-    }
+        setContentView(R.layout.interactive_map);AsyncTask<Void, Void, Bitmap> setImageFromUrl = new AsyncTask<Void, Void, Bitmap>(){
+            @Override
+            protected Bitmap doInBackground(Void... params) {
+                Bitmap bmp = null;
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet request = new HttpGet(STATIC_MAP_API_ENDPOINT);
 
-    public void centralMapButton(View view) {
-        startActivity(new Intent(this, CentralMap.class));
+                InputStream in = null;
+                try {
+                    in = httpclient.execute(request).getEntity().getContent();
+                    bmp = BitmapFactory.decodeStream(in);
+                    in.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return bmp;
+            }
+            protected void onPostExecute(Bitmap bmp) {
+                if (bmp!=null) {
+                    final ImageView iv = (ImageView) findViewById(R.id.img);
+                    iv.setImageBitmap(bmp);
+                }
+            }
+        };
+        setImageFromUrl.execute();
     }
 }
