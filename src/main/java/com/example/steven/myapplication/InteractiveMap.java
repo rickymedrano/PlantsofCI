bae5653467e78b14171141797cb04eebcef105f4
D0radus <allen.tyler.c@gmail.com>
2018-03-27 19:25:44 -0700

    Blank Map
    When clicking on interactive map, it now displays a blank google map.

--- src/main/java/com/example/steven/myapplication/InteractiveMap.java
+++ src/main/java/com/example/steven/myapplication/InteractiveMap.java
@@ -1,23 +1,19 @@
 package com.example.steven.myapplication;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.google.android.gms.maps.GoogleMap;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by Tyler on 3/5/2018.
  */
 
-public class InteractiveMap extends Activity
-{
-    GoogleMap mMap;
+public class InteractiveMap extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.interactive_map);
+
     }
+
 }
--- src/main/java/com/example/steven/myapplication/MainActivity.java
+++ src/main/java/com/example/steven/myapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.steven.myapplication;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
 
+@SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelperClass myDB;
+    DatabaseHelperClass myDB; //SQLite Database
+    private static final int ERROR_DIALOG_REQUEST = 9001; // used when requesting certain dialogue boxes
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
     }
     
     public void interactiveMapButton(View view) {
-        startActivity(new Intent(this, InteractiveMap.class));
+
+        // make sure google map services are working
+        if(servicesOk())
+        {
+            startActivity(new Intent(this, InteractiveMap.class));
+            Toast.makeText(this, "Ready to map!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void plantSearchButton(View view) {
@@ -25,5 +39,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void aboutButton(View view){
         startActivity(new Intent(this, AboutPage.class));
+    }
+
+    // make sure the google map service is good to use
+    public boolean servicesOk() {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
