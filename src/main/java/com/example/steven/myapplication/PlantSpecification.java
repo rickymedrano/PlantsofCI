09cc6fb7a1dbd713b9889da02597bea6f5b623a6
Ricky <rickymedrano4@gmail.com>
2018-04-09 10:38:38 -0700

    Actual update from master
    This is the actual update from master

--- src/main/java/com/example/steven/myapplication/PlantSpecification.java
+++ src/main/java/com/example/steven/myapplication/PlantSpecification.java
@@ -2,6 +2,9 @@ package com.example.steven.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class PlantSpecification extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class PlantSpecification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plant_specification);
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView2);
+        Picasso.get().load("https://docs.google.com/uc?id=1_NroIB9reMuUEJyxPVDN7ZjR4qG5DALO").into(imageView);
     }
 }
