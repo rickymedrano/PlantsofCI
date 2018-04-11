a8fe287de1dc4409f3ab766be2211b5326569fca
steven-romp <37082757+steven-romp@users.noreply.github.com>
2018-04-11 12:47:33 -0700

    update
    update to current master level, and to the main and the xml parser

--- src/main/java/com/example/stevenromp/plantsofci/MainActivity.java
+++ src/main/java/com/example/stevenromp/plantsofci/MainActivity.java
@@ -36,7 +36,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-
+        //Create input stream for XML file and pass into the parser (rempace the Null in parser(null)s
+        XMLParser test = new XMLParser();
+        try {
+            test.parse(null);//XML IN STREAM HERE);
+        }catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     public void interactiveMapButton(View view) {
@@ -72,42 +80,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-/*
-// created by Steven
-public class XMLParser {
-    private final String ns = null;
-
-    public List parse(InputStream in) throws XmlPullParserException, IOException {
-        try {
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(in, null);
-            parser.nextTag();
-            return readFeed(parser);
-        } finally {
-            in.close();
-        }
-    }
 
 
-    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-        List entries = new ArrayList();
 
-        parser.require(XmlPullParser.START_TAG, ns, "feed");
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
-            // Starts by looking for the entry tag
-            if (name.equals("entry")) {
-                entries.add(readEntry(parser));
-            } else {
-                skip(parser);
-            }
-        }
-        return entries;
-    }
-}
 
-*/
--- src/main/java/com/example/stevenromp/plantsofci/XMLParser.java
+++ src/main/java/com/example/stevenromp/plantsofci/XMLParser.java
@@ -87,7 +87,7 @@ public class XMLParser {
 
         // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them off
 // to their respective "read" methods for processing. Otherwise, skips the tag.
-        private com.example.stevenromp.plantsofci.XMLParser.Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
             parser.require(XmlPullParser.START_TAG, ns, "entry");
             String plantid = null;
             String commonname = null;
