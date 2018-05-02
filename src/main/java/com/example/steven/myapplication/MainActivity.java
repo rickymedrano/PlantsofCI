c5a35ae23d4e647ba8367afcef47f1e52d382c0b
steven-romp <37082757+steven-romp@users.noreply.github.com>
2018-05-02 12:29:27 -0700

    recent update to UI and code.

---
+++ src/main/java/com/example/steven/myapplication/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.steven.myapplication;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+@SuppressWarnings("ALL")
+public class MainActivity extends AppCompatActivity {
+    private static final int ERROR_DIALOG_REQUEST = 9001; // used when requesting certain dialogue boxes
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+    
+    public void interactiveMapButton(View view) {
+
+        // make sure google map services are working
+        if(servicesOk())
+        {
+            startActivity(new Intent(this, InteractiveMap.class));
+        }
+    }
+
+    public void plantSearchButton(View view) {
+        startActivity(new Intent(this, PlantSearch.class));
+    }
+
+    public void aboutButton(View view){
+        startActivity(new Intent(this, AboutPage.class));
+    }
+
+    // make sure the google map service is good to use
+    public boolean servicesOk() {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/example/steven/myapplication/PlantSpecification.java
@@ -0,0 +1,80 @@
+package com.example.steven.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+import org.xmlpull.v1.XmlPullParserException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class PlantSpecification extends AppCompatActivity implements OpenDatabase {
+    DatabaseSearch database;
+
+    @Override
+    public DatabaseSearch database() throws XmlPullParserException, IOException {
+        InputStream inputStream = getResources().openRawResource(R.raw.database);
+        XMLParser xmlParser = new XMLParser();
+        DatabaseSearch dataSearch = new DatabaseSearch(inputStream, xmlParser);
+        return dataSearch;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent recievedIntent = getIntent();
+        int plantID = recievedIntent.getIntExtra("plantID", 0);
+
+        try {
+            database = database();
+
+        } catch (XmlPullParserException error) {
+            error.printStackTrace();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+
+        setContentView(R.layout.plant_specification);
+
+        //Set Common Name
+        TextView plantTitle = findViewById(R.id.plantTitle);
+        plantTitle.setText(database.getFullDatabase().get(plantID).getCommonName().getObj());
+        //Set Species Name
+        TextView plantSpecies = findViewById(R.id.speciesname);
+        plantSpecies.setText(database.getFullDatabase().get(plantID).getSpeciesName().getObj());
+        //Set Location
+        TextView plantLocation = findViewById(R.id.location);
+        String[] location = database.getFullDatabase().get(plantID).getLocation().getObj();
+        StringBuilder stringLocation = new StringBuilder();
+        for (int i = 0; i < location.length; i++)
+        {
+            stringLocation.append(location[i]);
+        }
+        plantLocation.setText(stringLocation);
+        //Set Flower Color
+        TextView plantFlowerColor = findViewById(R.id.flowercolor);
+        plantFlowerColor.setText(database.getFullDatabase().get(plantID).getFlowerColor().getObj());
+        //Set Origin
+        TextView plantOrigin = findViewById(R.id.origin);
+        plantOrigin.setText(database.getFullDatabase().get(plantID).getOrigin().getObj());
+        //Set Bloom Season
+        TextView plantBloomSeason = findViewById(R.id.bloomseason);
+        plantBloomSeason.setText(database.getFullDatabase().get(plantID).getBloomSeason().getObj());
+        //Set Drought Tolerance
+        TextView plantDroughtTolerance = findViewById(R.id.drought);
+        plantDroughtTolerance.setText(database.getFullDatabase().get(plantID).getDrought().getObj());
+//        //Set Height
+        TextView plantHeight = findViewById(R.id.plantHeight);
+        plantHeight.setText(database.getFullDatabase().get(plantID).getPlantHeight().getObj());
+        //Set Width
+        TextView plantWidth = findViewById(R.id.plantWidth);
+        plantWidth.setText(database.getFullDatabase().get(plantID).getPlantWidth().getObj());
+        //Set Image
+        ImageView imageView = (ImageView)findViewById(R.id.imageView2);
+        Picasso.get().load("file:///android_asset/PlantPictures/" + Integer.toString(plantID+1) + "-1.png").placeholder(R.drawable.cast_album_art_placeholder).into(imageView);
+
+    }
+}
